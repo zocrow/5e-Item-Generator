ff743f57e380a467e58f3f3f08872642864c9ce2
zocrow <sethashton@msn.com>
2024-08-20 17:02:54 -0400

    classitems.json
    Early push to get classitems.json as a web file.

--- functions.js
+++ functions.js
@@ -1,4 +1,13 @@
-generate()
+var json = require('./data.json');
+function generate(feClass)
 {
-    
+    document.getElementById('result').innerHTML = "bye there";
+    if (feClass.localeCompare("Artificer") == 0)
+    {
+        document.getElementById('result').innerHTML = "artificer items yippee!!!";
+    }
+    else
+    {
+        alert('you didnt pick artificer');
+    }
 }
